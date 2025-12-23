e995ce0a2a64c2575acbf5b55da738c0d3b3b89c
avish006 <167643918+avish006@users.noreply.github.com>
2025-12-24 00:25:07 +0530

    Enhance schema_creation.sql with detailed comments
    Added comments explaining the purpose of each schema.

--- schema_creation.sql
+++ schema_creation.sql
@@ -1,5 +1,6 @@
 -- Creation of DB Schemas for our DWH
+-- These are the 3 layers of data storage
 
-create schema bronze;
-create schema silver;
-create schema gold;
+create schema bronze; -- Responsible for storing every data without preprocessinig or cleaning
+create schema silver; -- Responsible for doing data cleaning, making transformation to suit different requirements
+create schema gold; -- Last layer responsible for structurally presenting of data to business stakeholders and non-tech people to perform insight driven business action
